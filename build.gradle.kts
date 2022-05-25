445d4abcff70ce0fdff159651d3aec1f9e85d9e0
Marcus Moore <contact@marcusmoore.io>
2022-05-25 13:36:41 -0700

    Migrate to Kotlin script for build configuration

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+fun properties(key: String) = project.findProperty(key).toString()
+
+plugins {
+    // Java support
+    id("java")
+    // Kotlin support
+    id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    // Gradle IntelliJ Plugin
+    id("org.jetbrains.intellij") version "1.6.0"
+}
+
+// Configure project's dependencies
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
+intellij {
+    version.set(properties("intellijPlatformVersion"))
+}
+
+group = properties("myPluginGroup")
+version = properties("myPluginVersion")
