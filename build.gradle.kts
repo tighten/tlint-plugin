6ef304c723f5aebc17295b0a327c0504049c36f8
Marcus Moore <contact@marcusmoore.io>
2022-05-26 17:41:10 -0700

    Add patchPluginXml task to build

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,4 +46,10 @@ tasks {
     wrapper {
         gradleVersion = properties("gradleVersion")
     }
+
+    patchPluginXml {
+        version.set(properties("pluginVersion"))
+        sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
+    }
 }
